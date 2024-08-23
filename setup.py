b0f0f4d95d392a982ee70ccf999291ae6f2947ed
Paulo César <pauloguitarrasoms@gmail.com>
2024-08-23 04:28:41 -0300

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Lê o conteúdo do README.md
-with open('READMELIB.md', 'r', encoding='utf-8') as file:
+with open('README.md', 'r', encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
